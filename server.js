d59c8f0f6a7199da92fff74cd742c41fb70aa69c
Unknown <michael.brouchoud@heig-vd.ch>
2018-12-13 22:29:38 +0100

    cors le cors

--- server.js
+++ server.js
@@ -27,23 +27,18 @@ const {
 
 const app = express();
 
-// Set up a whitelist and check against it:
-var whitelist = [process.env.WHITELIST_HOST]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/api', auth);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 /* istanbul ignore if  */
 if (process.env.NODE_MODE !== 'test') {
